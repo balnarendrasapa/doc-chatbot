b04b4727cd1e006cff6a7d30322bd95baaa6bbdd
Bal Narendra Sapa <bnsapa2000@gmail.com>
2023-11-01 16:29:09 -0400

    add title

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 
 def app():
-    st.set_page_config(page_title="Chat with Documents")
+    st.set_page_config(page_title="Chat with AI", page_icon="🤖", layout="centered")
+    st.title("Chat with AI")
     Option = st.selectbox(
         "Select an option",
         ("Select", "HuggingFace", "OpenAI")
