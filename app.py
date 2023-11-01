596f71e8b43728a5abf933203fd3525a5997d757
Bal Narendra Sapa <bnsapa2000@gmail.com>
2023-11-01 16:21:21 -0400

    add select option

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 def app():
     st.set_page_config(page_title="Chat with Documents")
-    Option = st.select_box("Select an option", ("HuggingFace", "OpenAI"))
+    Option = st.selectbox("Select an option", ("Select", "HuggingFace", "OpenAI"))
 
 
 app()
