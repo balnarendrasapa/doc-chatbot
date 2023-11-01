a967574639e01c08ff31f95367af7fd4154b380e
Bal Narendra Sapa <bnsapa2000@gmail.com>
2023-11-01 16:23:33 -0400

    add uploader method

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import streamlit as st
 def app():
     st.set_page_config(page_title="Chat with Documents")
     Option = st.selectbox("Select an option", ("Select", "HuggingFace", "OpenAI"))
-
+    if Option != "Select":
+        doc = st.file_uploader("Upload a document", type=["pdf"])
 
 app()
 
