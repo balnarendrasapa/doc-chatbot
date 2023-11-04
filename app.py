be87b7a72cdf13201c9e16d553e4f92d0dd82877
Bal Narendra Sapa <bnsapa2000@gmail.com>
2023-11-04 15:33:12 -0400

    Add chat functionality to app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.chains import RetrievalQA
 from langchain.llms import OpenAI
 from langchain.llms import HuggingFaceHub
+import time
 # from langchain import HuggingFaceHub
 
 if "messages" not in st.session_state:
@@ -66,6 +67,31 @@ def app():
                         st.write(qa(Question))
         if PART_B:
             part_a.empty()
+            if prompt := st.chat_input("What is up?"):
+                st.session_state.messages.append(
+                    {
+                        "role": "user",
+                        "content": prompt
+                    }
+                )
+                with st.chat_message("user"):
+                    st.markdown(prompt)
+
+                with st.chat_message("assistant"):
+                    message_placeholder = st.empty()
+                    full_response = ""
+                    assistant_response = qa(prompt)
+                    for chunk in assistant_response.split():
+                        full_response += chunk + " "
+                        time.sleep(0.05)
+                        message_placeholder.markdown(full_response + "▌")
+                    message_placeholder.markdown(full_response)
+                st.session_state.messages.append(
+                    {
+                        "role": "assistant",
+                        "content": full_response
+                    }
+                )
 
 
 if __name__ == "__main__":
