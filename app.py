d949bcd42f31f9024fcaac0973eb04ac9091da46
Bal Narendra Sapa <bnsapa2000@gmail.com>
2023-11-04 14:48:37 -0400

    add llm code

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from langchain.document_loaders import PyPDFLoader
 from langchain.vectorstores import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.chains import RetrievalQA
+from langchain.llms import OpenAI
 
 
 def app():
@@ -16,21 +18,32 @@ def app():
         ("Select", "HuggingFace", "OpenAI")
     )
     if Option != "Select":
-        doc = st.file_uploader("Upload a document", type=["pdf"])
-        if doc is not None:
-            with open("doc.pdf", "wb") as f:
-                f.write(doc.getbuffer())
-            loader = PyPDFLoader("doc.pdf")
-            pages = loader.load_and_split()
-            embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-            faiss_index = FAISS.from_documents(pages, embeddings)
-            Question = st.text_input("Ask a question")
-            if Question != "":
-                docs = faiss_index.similarity_search(Question, k=5)
-                for doc in docs:
-                    st.write(
-                        str(doc.metadata["page"]) + ":", doc.page_content[:300]
-                    )
+        API = st.text_input("Enter your " + Option + " API key")
+        if API != "":
+            doc = st.file_uploader("Upload a document", type=["pdf"])
+            if doc is not None:
+                with open("doc.pdf", "wb") as f:
+                    f.write(doc.getbuffer())
+                loader = PyPDFLoader("doc.pdf")
+                pages = loader.load_and_split()
+                embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+                faiss_index = FAISS.from_documents(pages, embeddings)
+                llm = OpenAI(api_key=API)
+                qa = RetrievalQA.from_chain_type(
+                    llm=llm,
+                    chain_type="stuff",
+                    retriever=faiss_index.as_retriever(
+                        search_type="mmr",
+                        search_kwargs={'fetch_k': 10}),
+                    return_source_documents=True
+                )
+                Question = st.text_input("Ask a question")
+                if Question != "":
+                    docs = faiss_index.similarity_search(Question, k=5)
+                    for doc in docs:
+                        st.write(
+                            str(doc.metadata["page"]) + ":", doc.page_content[:300]
+                        )
 
 
 if __name__ == "__main__":
